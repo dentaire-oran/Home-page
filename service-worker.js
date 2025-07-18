c83f5c0ce37a2a825a0a44173c3bb38c762768e1
dentaire-oran <benosmanzeriab@gmail.com>
2025-07-18 14:42:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,23 @@
-const CACHE_NAME = 'dental-guide-v1';
-
-// Liste des fichiers à mettre en cache pour "Your Dental Guide"
+const CACHE_NAME = 'dental-guide-v2'; // J'ai changé la version pour forcer la mise à jour
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/Cours/index.html',
-  '/Qcm/index.html',
-  '/Moyenne/index.html',
-  '/images/icon-192x192.png',
-  '/images/icon-512x512.png'
+  './',
+  'index.html',
+  'Cours/index.html',
+  'Qcm/index.html',
+  'Moyenne/index.html',
+  'images/icon-192x192.png',
+  'images/icon-512x512.png'
 ];
 
-// Installe le Service Worker et met les fichiers en cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Cache ouvert');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Récupère le contenu depuis le cache s'il est disponible, sinon depuis le réseau
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
